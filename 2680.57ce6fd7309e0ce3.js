277a7538f2404137bbaf3b889c634f05a29a3fda
Dirk Gausmann <di.gaus@gmx.de>
2022-10-25 23:18:27 +0200

    Update

---
+++ 2680.57ce6fd7309e0ce3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[2680],{2680:(r,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>u});var s=e(9671),_=e(7423);class u extends _.Uw{show(o){return(0,s.Z)(function*(){})()}hide(o){return(0,s.Z)(function*(){})()}}}}]);
